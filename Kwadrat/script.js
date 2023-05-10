8621a918f8e61fd3802471c8bbd2290db100ab6c
Kacper Tatrocki <102294177+Tatraa@users.noreply.github.com>
2023-05-10 23:08:21 +0200

    Kwadrat

---
+++ Kwadrat/script.js
@@ -0,0 +1,101 @@
+const vertexShaderTxt = `
+    precision mediump float;
+
+    attribute vec2 vertPosition;
+    attribute vec3 vertColor;
+
+    varying vec3 fragColor;
+
+    void main()
+    {
+        fragColor = vertColor;
+        gl_Position = vec4(vertPosition, 0.0, 1.0);
+    }
+`;
+
+const fragmentShaderTxt = `
+    precision mediump float;
+
+    varying vec3 fragColor;
+
+    void main()
+    {
+        gl_FragColor = vec4(fragColor, 1.0);
+    }
+`;
+
+const Square = function () {
+    const canvas = document.getElementById("main-canvas");
+    const gl = canvas.getContext("webgl");
+
+    if (!gl) {
+        alert('no webgl');
+    }
+
+    gl.clearColor(0.5, 0.5, 0.9, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+
+    gl.shaderSource(vertexShader, vertexShaderTxt);
+    gl.shaderSource(fragmentShader, fragmentShaderTxt);
+
+    gl.compileShader(vertexShader);
+    if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
+        console.log(gl.getShaderInfoLog(vertexShader));
+    }
+    gl.compileShader(fragmentShader);
+
+    const program = gl.createProgram();
+
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+
+    gl.detachShader(program, vertexShader);
+    gl.detachShader(program, fragmentShader);
+
+    gl.validateProgram(program);
+
+    let squareVertices = [
+        // X, Y         RGB
+        -0.5, 0.5,     1.0, 0.0, 0.0, // Top Left
+        0.5, 0.5,      0.0, 1.0, 0.0, // Top Right
+        -0.5, -0.5,    0.0, 0.0, 1.0, // Bottom Left
+        0.5, -0.5,     1.0, 1.0, 0.0  // Bottom Right
+    ];
+
+    const squareVertexBufferObject = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexBufferObject);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(squareVertices), gl.STATIC_DRAW);
+
+    const posAttrLocation = gl.getAttribLocation(program, 'vertPosition');
+    const colorAttrLocation = gl.getAttribLocation(program, 'vertColor');
+
+    gl.vertexAttribPointer(
+        posAttrLocation,
+        2,
+        gl.FLOAT,
+        gl.FALSE,
+        5 * Float32Array.BYTES_PER_ELEMENT,
+        0
+    );
+
+    gl.vertexAttribPointer(
+        colorAttrLocation,
+        3,
+        gl.FLOAT,
+        gl.FALSE,
+        5 * Float32Array.BYTES_PER_ELEMENT,
+        2 * Float32Array.BYTES_PER_ELEMENT
+    );
+
+    gl.enableVertexAttribArray(posAttrLocation);
+    gl.enableVertexAttribArray(colorAttrLocation);
+
+    gl.useProgram(program);
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+};
+
+Square();
